e63189efca553dfe959f1ccf5c89cdb9b75fdba6
Santiago Aguirre <aguirre1293@hotmail.com>
2024-01-25 15:57:27 +0100

    Implement Bubble Sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < i; ++j) {
+            if (arr[j] > arr[i]) {
+                const tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+            }
+        }
+    }
 }
