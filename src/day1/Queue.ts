d8db94b05ebaf75cbda8080847aa49d1f740baa1
Santiago Aguirre <aguirre1293@hotmail.com>
2024-01-26 08:51:29 +0100

    Implement Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+interface Node<T> {
+    value: T,
+    next?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>
+    private tail?: Node<T>
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0
     }
 
     enqueue(item: T): void {
+        const newNode = {value: item} as Node<T>
+        this.length++
+        if (!this.tail) {
+            this.head = newNode
+            this.tail = newNode
+            return
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
+        const head = this.head
+        this.length--;
 
-}
+        this.head = this.head.next
+        head.next = undefined
+
+        if(this.length === 0) {
+            this.tail = this.head
+        }
+
+        return head.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
