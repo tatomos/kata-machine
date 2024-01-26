a886079a7264424ea369646f7204ee03892c7ffd
Santiago Aguirre <aguirre1293@hotmail.com>
2024-01-26 09:05:08 +0100

    Implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,46 @@
+interface Node<T> {
+    value: T;
+    prev?: Node<T>;
+}
+
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>
 
     constructor() {
+        this.length = 0
+        this.head = undefined
     }
 
     push(item: T): void {
 
-}
+        const node = {value: item} as Node<T>
+
+        this.length++;
+
+        if (!this.head) {
+            this.head = node
+            return
+        }
+
+        node.prev = this.head
+        this.head = node
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
 
-}
+        this.length--;
+
+        const head = this.head
+        this.head = this.head.prev
+        return head.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
